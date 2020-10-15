77cece1ceb69bc236f70b9b3b0398489b2a261a8
tomcatmew <helltomcat@gmail.com>
2020-10-15 20:16:12 +0800

    modified main

--- one_triangle/main.cpp
+++ one_triangle/main.cpp
@@ -31,7 +31,7 @@ int main(void)
   glfwSetErrorCallback(error_callback);
   if (!glfwInit())
     exit(EXIT_FAILURE);
-  window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+  window = glfwCreateWindow(640, 680, "Simple example", NULL, NULL);
   if (!window)
   {
     glfwTerminate();
@@ -52,14 +52,14 @@ int main(void)
     glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    //glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
     glBegin(GL_TRIANGLES);
-    glColor3f(1.f, 0.f, 0.f);
+    glColor3f(1.f, 0.f, 0.5f);
     glVertex3f(-0.6f, -0.4f, 0.f);
-    glColor3f(0.f, 1.f, 0.f);
+    glColor3f(1.f, 0.f, 0.5f);
     glVertex3f(0.6f, -0.4f, 0.f);
-    glColor3f(0.f, 0.f, 1.f);
-    glVertex3f(0.f, 0.6f, 0.f);
+    glColor3f(1.f, 0.5f, 0.f);
+    glVertex3f(0.f, 0.8f, 0.f);
     glEnd();
     glfwSwapBuffers(window);
     glfwPollEvents();
