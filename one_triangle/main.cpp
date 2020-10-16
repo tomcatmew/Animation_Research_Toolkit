cac7356dc2c5b945c2d5d92f62c5735086f60d28
tomcatmew <helltomcat@gmail.com>
2020-10-17 00:57:47 +0800

    update main.cpp in draw triangle

--- one_triangle/main.cpp
+++ one_triangle/main.cpp
@@ -10,6 +10,9 @@
  * @brief this demo just open window and it doesn't use DelFEM2 library
  */
 
+
+// Modified by Yifei Chen for OpenGL review and learning
+
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <cstdio>
@@ -31,20 +34,23 @@ int main(void)
   glfwSetErrorCallback(error_callback);
   if (!glfwInit())
     exit(EXIT_FAILURE);
-  window = glfwCreateWindow(640, 680, "Simple example", NULL, NULL);
+  window = glfwCreateWindow(840, 680, "Simple example", NULL, NULL);
   if (!window)
   {
     glfwTerminate();
     exit(EXIT_FAILURE);
   }
+
   glfwMakeContextCurrent(window);
   glfwSetKeyCallback(window, key_callback);
+
   while (!glfwWindowShouldClose(window))
   {
     float ratio;
     int width, height;
     glfwGetFramebufferSize(window, &width, &height);
     ratio = width / (float) height;
+
     glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
     glMatrixMode(GL_PROJECTION);
@@ -53,14 +59,32 @@ int main(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     //glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.f, 0.f, 0.5f);
+    glBegin(GL_POLYGON);
+    glColor3f(0.7f, 1.f, 0.f);
     glVertex3f(-0.6f, -0.4f, 0.f);
-    glColor3f(1.f, 0.f, 0.5f);
+
+    glColor3f(0.7f, 1.f, 0.f);
     glVertex3f(0.6f, -0.4f, 0.f);
-    glColor3f(1.f, 0.5f, 0.f);
+
+    glColor3f(0.1f, 1.f, 1.f);
+    glVertex3f(0.8f, 0.2f, 0.f);
+
+    glColor3f(0.1f, 1.f, 1.f);
     glVertex3f(0.f, 0.8f, 0.f);
+
+    glColor3f(0.1f, 1.f, 1.f);
+    glVertex3f(-0.8f, 0.2f, 0.f);
     glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    glColor3f(1.f, 1.f, 1.f);
+    glVertex3f(-0.9f, -0.8f, 0.f);
+    glVertex3f(0.9f, -0.8f, 0.f);
+    glVertex3f(0.9f, 0.9f, 0.f);
+    glVertex3f(-0.9f, 0.9f, 0.f);
+    glVertex3f(-0.9f, -0.8f, 0.f);
+    glEnd();
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
