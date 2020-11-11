f5cb5667d38a62f3504d487e4df031fe7d38a93f
tomcatmew <helltomcat@gmail.com>
2020-11-11 20:09:19 +0800

    update cube code

--- draw_cuboid/main.cpp
+++ draw_cuboid/main.cpp
@@ -1,85 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <glad/glad.h>
-// Include GLFW
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-using namespace glm;
-
-#include <shader.hpp>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
 #include <iostream>
-int main(void)
-{
-	// Initialise GLFW
-	if (!glfwInit())
-	{
-		fprintf(stderr, "Failed to initialize GLFW\n");
-		getchar();
-		return -1;
-	}
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// Open a window 
-	window = glfwCreateWindow(1024, 768, "Draw Cuboid and transformation", NULL, NULL);
-	if (window == NULL) {
-		fprintf(stderr, "Failed to open GLFW window.\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
-
-	// Dark blue 
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-
-	glEnable(GL_DEPTH_TEST);
-
-	glDepthFunc(GL_LESS);
-
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader");
-
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-
-	// Camera program. from internet =====================================================
-	// Projection matrix : 45?Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	glm::mat4 View = glm::lookAt(
-		glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
-		glm::vec3(0, 0, 0), // and looks at the origin
-		glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-	);
-
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
-	//above camera program ================================================================
-
-
-	static const GLfloat g_vertex_buffer_data[] = {
+void geneCUBE(std::vector<double>& out_vertices, std::vector<unsigned int>& out_triangles) {
+	out_vertices = {
 		-1.0f,-1.0f,-1.0f,
 		-1.0f,-1.0f, 2.0f,
 		-1.0f, 1.0f, 2.0f,//1
@@ -118,102 +44,142 @@ int main(void)
 		 1.0f,-1.0f, 2.0f//12
 	};
 
-	//color for vertex
-	static const GLfloat g_color_buffer_data[] = {
-		0.583f,  0.771f,  0.014f,
-		0.609f,  0.115f,  0.436f,
-		0.327f,  0.483f,  0.844f,
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-		0.597f,  0.770f,  0.761f,
-		0.559f,  0.436f,  0.730f,
-		0.359f,  0.583f,  0.152f,
-		0.483f,  0.596f,  0.789f,
-		0.559f,  0.861f,  0.639f,
-		0.195f,  0.548f,  0.859f,
-		0.014f,  0.184f,  0.576f,
-		0.771f,  0.328f,  0.970f,
-		0.406f,  0.615f,  0.116f,
-		0.676f,  0.977f,  0.133f,
-		0.971f,  0.572f,  0.833f,
-		0.140f,  0.616f,  0.489f,
-		0.997f,  0.513f,  0.064f,
-		0.945f,  0.719f,  0.592f,
-		0.543f,  0.021f,  0.978f,
-		0.279f,  0.317f,  0.505f,
-		0.167f,  0.620f,  0.077f,
-		0.347f,  0.857f,  0.137f,
-		0.055f,  0.953f,  0.042f,
-		0.714f,  0.505f,  0.345f,
-		0.783f,  0.290f,  0.734f,
-		0.722f,  0.645f,  0.174f,
-		0.302f,  0.455f,  0.848f,
-		0.225f,  0.587f,  0.040f,
-		0.517f,  0.713f,  0.338f,
-		0.053f,  0.959f,  0.120f,
-		0.393f,  0.621f,  0.362f,
-		0.673f,  0.211f,  0.457f,
-		0.820f,  0.883f,  0.371f,
-		0.982f,  0.099f,  0.879f
-	};
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-	GLuint colorbuffer;
-	glGenBuffers(1, &colorbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-
-	//!!! write affine matrix transfomation here ------------------------------------------------------------
-	glm::mat4 trans;
-
-
-	do {
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		glUseProgram(programID);
-
-
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-		//  vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(0,3, GL_FLOAT,GL_FALSE,0,(void*)0);
-
-		// colors
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-		glVertexAttribPointer(1,3, GL_FLOAT,GL_FALSE,0,(void*)0);
-
-		// !!!!calculate transformation here ---------------------
-
-		// Draw the triangle
-		glDrawArrays(GL_TRIANGLES, 0, 12 * 3); // 12*3 indices
-
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-	} // Check if the ESC key was pressed or the window was closed
-	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
-		glfwWindowShouldClose(window) == 0);
-
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteBuffers(1, &colorbuffer);
-	glDeleteProgram(programID);
-	glDeleteVertexArrays(1, &VertexArrayID);
-
-	// Close  window 
-	glfwTerminate();
-
-	return 0;
+	for (int i = 0; i < 36; i++)
+	{
+		out_triangles.push_back(i);
+	}
+	std::cout << out_triangles.size() << std::endl;
 }
 
+bool loadOBJ(
+	const char* path,
+
+	std::vector<double>& out_vertices,
+	std::vector<unsigned int>& out_triangles
+) {
+	printf("Loading OBJ file %s...\n", path);
+
+
+	//std::vector<double> temp_vertices;
+	std::vector<unsigned int> vertexIndices;
+
+	FILE* file = fopen(path, "r");
+	if (file == NULL) {
+		printf("cannot open the file !\n");
+		getchar();
+		return false;
+	}
+
+	while (1) {
+
+		char lineHeader[128];
+		// read the first word 
+		int res = fscanf(file, "%s", lineHeader);
+		if (res == EOF)
+			break; // EOF = End Of File Quit 
+		// else : parse lineHeader
+
+		if (strcmp(lineHeader, "v") == 0) {
+			double x, y, z;
+			fscanf(file, "%lf %lf %lf\n", &x, &y, &z);
+			std::cout << x << " " << y << " " << z << " " << std::endl;
+			out_vertices.push_back(x);
+			out_vertices.push_back(y);
+			out_vertices.push_back(z);
+		}
+		else if (strcmp(lineHeader, "f") == 0) {
+			unsigned int vertexIndex[3];
+			int matches = fscanf(file, "%d %d %d/\n", &vertexIndex[0], &vertexIndex[1], &vertexIndex[2]);
+			if (matches != 3) {
+				printf("File can't be read");
+				fclose(file);
+				return false;
+			}
+			//std::cout << vertexIndex[0] << " " << vertexIndex[1] << " " << vertexIndex[2] << " " << std::endl;
+			vertexIndices.push_back(vertexIndex[0] - 1 );
+			vertexIndices.push_back(vertexIndex[1] - 1);
+			vertexIndices.push_back(vertexIndex[2] - 1);
+		}
+		else {
+			// comment
+			char stupidBuffer[1000];
+			fgets(stupidBuffer, 1000, file);
+		}
+
+	}
+	//std::cout << temp_vertices[0] << " " << temp_vertices[1] << " " << temp_vertices[2] << std::endl;
+	std::cout << "\n print vertices  index is \n";
+	std::cout << vertexIndices.size() << std::endl;
+
+	out_triangles = vertexIndices;
+	fclose(file);
+	return true;
+}
+
+static void error_callback(int error, const char* description)
+{
+  fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+
+int main(void)
+{
+	std::vector<double> points;
+	std::vector<unsigned int> indexs;
+	//loadOBJ("bunny.obj", points, indexs);
+	geneCUBE(points, indexs);
+
+  GLFWwindow* window;
+  glfwSetErrorCallback(error_callback);
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+  window = glfwCreateWindow(840, 680, "Assignment_3_transform_cuboid", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, key_callback);
+  int iframe = 0;
+  while (!glfwWindowShouldClose(window))
+  {
+    float ratio;
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float) height;
+
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio*5.f, ratio * 5.f, -1.f * 5.f, 1.f * 5.f, 1.f * 5.f, -1.f * 5.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    //glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+	glColor3f(1.0f,1.0f,1.0f);
+	for (unsigned int i = 0; i < indexs.size() / 3; ++i) {
+
+		unsigned int i0 = indexs[i * 3 + 0];
+		unsigned int i1 = indexs[i * 3 + 1];
+		unsigned int i2 = indexs[i * 3 + 2];
+		glVertex3f(points[i0 * 3 + 0], points[i0 * 3 + 1], points[i0 * 3 + 2]);
+		glVertex3f(points[i1 * 3 + 0], points[i1 * 3 + 1], points[i1 * 3 + 2]);
+		glVertex3f(points[i2 * 3 + 0], points[i2 * 3 + 1], points[i2 * 3 + 2]);
+	}
+		glEnd();
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
+}
